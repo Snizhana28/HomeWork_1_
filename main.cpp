e6303ca1dcb865e8dc65845a6192793f31d748f2
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 14:41:58 +0300

    Done Task 1.4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+/*Task 4. Find the product of all integers from a to 20 (the value of a is entered from the keyboard: 1 <= a <= 20)*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int a;
+	cout << "Enter 1 number of the range (1 <= a <= 20) : ";
+	cin >> a;
+	long long result = 1;
+	for (int i = a; i <= 20; i++)
+		result *= i;
+	cout << "Result : " << result;
 	return 0;
 }
