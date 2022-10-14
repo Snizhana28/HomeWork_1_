119a2475f9864d7bc414e65c1b0a0c68128167c2
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 15:57:34 +0300

    Done Task 2.4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+/*Task 4. The user enters any integer A.
+It is necessary to derive all integers B for which A is divisible
+without a remainder on B*B and is not divisible without a remainder on B*B*B.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int A;
+	cout << "Enter a number : ";
+	cin >> A;
+	
+	for (int B = 1; B < A; B++)
+	{
+		if (A % (B * B) == 0 && A % (B * B * B) != 0)
+			cout << "Numbers : " << B << "  ";
+	}
 	return 0;
 }
