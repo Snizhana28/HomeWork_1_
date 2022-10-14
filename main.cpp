e006323c896c85f0ab9d8a109a5462028415af59
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 15:28:44 +0300

    Done Task 2.2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+/*Task 2. Count the number of integers in the range
+from 100 to 999, in which all digits are different.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int suma = 0;
+	for (int i = 100; i <= 999; i++)
+	{
+		int n1 = i / 100;
+		int n2 = (i / 10) % 10;
+		int n3 = i % 10;
+		if (n1 != n2 && n2 != n3 && n1 != n3)
+		{
+			suma++;
+		}
+	}
+	cout << "Suma : " << suma;
 	return 0;
 }
