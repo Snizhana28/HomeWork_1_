021ea9195349d66e46d2033c2f4f1c98da13abe6
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 16:41:31 +0300

    Done Task 2.6

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+/*Task 6. The user enters an integer. Necessary
+output all integers by which the given number is divisible by no
+the remainder*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int x;
+	cout << "Enter a number : ";
+	cin >> x;
+	if (x > 0)
+	{
+		for (int i = 1; i <= x ; i++)
+		{
+			if (x % i == 0)
+				cout << i << "  ";
+		}
+	}
+	else
+	  {
+	  	cout << "Error";
+	  }
+	cout << endl;
 	return 0;
 }
