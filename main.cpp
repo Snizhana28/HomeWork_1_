0aa6ec79a61134fb23951fbe2ac6df5f2e64c007
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 15:24:40 +0300

    Done Task 2.1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
+/*Task 1. Count the number of integers in the range
+from 100 to 999 that have two identical digits.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int suma = 0;
+	for (int i = 100; i <= 999; i++)
+	{
+		int n1 = i / 100;
+		int n2 = (i / 10) % 10;
+		int n3 = i % 10;
+		if (n1 == n2 && n1 != n3 ||
+			n1 == n3 && n1 != n2 ||
+			n2 == n3 && n1 != n2)
+		{
+			suma++;
+		}
+	}
+		cout << "Suma : " << suma;
 	return 0;
 }
