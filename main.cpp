d276da21bc74b1486dbdba6e4e3a0b0e82634ed6
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 14:27:07 +0300

    Done Task 1.3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+/*Find the arithmetic mean of all integers
+from 1 to 1000.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	double result = 0;
+	double suma = 0;
+	for (int i = 1; i <= 1000; i++)
+	{
+		suma += i;
+		result = (suma) / 1000;
+	}
+	cout << "Result : " << result;
 	return 0;
 }
