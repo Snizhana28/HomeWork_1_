2535855847a8dd91d0c1f0b742ef35533d96adbb
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 14:31:21 +0300

    Done Task 1.1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+/*Task 1. Write a program that calculates the sum of integers
+numbers from a to 500 (the value of a is entered from the keyboard).*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int a;
+	cout << "Enter a number : ";
+	cin >> a;
+	int suma = 0;
+	int i = a;
+	while (i <= 500)
+	{
+		suma += i;
+		i++;
+	}
+	cout << "Suma : " << suma;
 	return 0;
 }
