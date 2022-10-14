ba2ab56e9004d27dc4db4a60ec667833df5c983a
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-14 14:44:19 +0300

    Done Task 1.5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+/*Task 5. Write a program that displays on the screen
+table of multiplication by k, where k is the option number.*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int k;
+	cout << "Enter a number for the multiplication table : ";
+	cin >> k;
+	int result = 0;
+	if (k >= 1 && k <= 9)
+	{
+		for (int i = 1; i <= 9; i++)
+		{
+			cout << k << " * " << i << " = " << k * i << endl;
+		}
+	}
+	else 
+	{
+		cout << "Error";
+	}
 	return 0;
 }
